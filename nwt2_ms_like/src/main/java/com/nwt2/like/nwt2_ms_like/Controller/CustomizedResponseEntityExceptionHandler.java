abf6cc232a75ff07c7e4e0c085bd2acb3e072809
aminap <spirala4wt@hotmail.com>
2018-03-26 00:35:32 +0200

    Error handling change

--- nwt2_ms_like/src/main/java/com/nwt2/like/nwt2_ms_like/Controller/CustomizedResponseEntityExceptionHandler.java
+++ nwt2_ms_like/src/main/java/com/nwt2/like/nwt2_ms_like/Controller/CustomizedResponseEntityExceptionHandler.java
@@ -9,17 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Date;
-
 @ControllerAdvice
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request) {
-        ErrorDetails errorDetails = new ErrorDetails(new Date(), "Validation Failed",
-                ex.getBindingResult().getFieldError().getDefaultMessage().toString());
+        CustomErrorType errorDetails = new CustomErrorType(ex.getBindingResult().getFieldError().getDefaultMessage().toString());
         return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
