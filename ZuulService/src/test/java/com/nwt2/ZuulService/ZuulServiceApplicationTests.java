1a944e09d4ad413c98854fcf81ce91781c38c31f
vildanap <vpanjeta1@etf.unsa.ba>
2018-05-08 13:26:43 +0200

    Zuul Service

---
+++ ZuulService/src/test/java/com/nwt2/ZuulService/ZuulServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nwt2.ZuulService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
